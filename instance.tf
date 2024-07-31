7790de7fbf4a36d4227b964083fbfc198e410301
sonamgaikwad <sonamgaikwad1852@gmail.com>
2024-07-31 19:21:56 +0530

    modified new file

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance1" {
     ami = "0b8fd93c15b2c81ce"
     instance_type = "t3.micro"
     key_name = "asus"
-    vpc_security_group_ids = "default"
+    vpc_security_group_ids = "sg-03b3dbd6988fde15a"
 
     tags = {
       name = "my_first_instance"
