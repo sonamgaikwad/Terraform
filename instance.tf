9c43c35a40a74cd742e2a3eed8634e65b53d50f3
sonamgaikwad <sonamgaikwad1852@gmail.com>
2024-07-31 19:13:07 +0530

    add instance.tf

---
+++ instance.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "eu-north-1"
+    profile = "configs"
+  
+}
+
+resource "aws_instance" "instance1" {
+    ami = "0b8fd93c15b2c81ce"
+    instance_type = "t3.micro"
+    key_name = "asus"
+    vpc_security_group_ids = "default"
+
+    tags = {
+      name = "my_first_instance"
+    }
+  
+}
